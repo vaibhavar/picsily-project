78111195ca26a17cc147376f1acdd81b6984b910
Vaibhav Arora <vaibhav.arora@sap.com>
2017-12-26 20:43:00 +0530

    Added process.env

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : ENV['MONGODB_URI'] //'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : process.env['MONGODB_URI'] //'mongodb://localhost:27017/conFusion'
 }
