de002fa3547a8185dcc139379e6c1c388b8deacc
vaibhavar <webdevpls@gmail.com>
2018-03-08 12:17:26 +0530

    Added CORS for picsily-ui app

--- app.js
+++ app.js
@@ -9,16 +9,13 @@ var LocalStrategy = require('passport-local').Strategy;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var photosRouter = require("./routes/photosRouter");
+var photosRouter = require('./routes/photosRouter');
 
+var config = require('./config');
 
-var config = require("./config");
-
-console.log("Config = ",config);
+console.log('Config = ', config);
 
 var app = express();
-
-
 
 // Connection to MongoDB server
 var mongoose = require('mongoose');
@@ -26,11 +23,10 @@ var mongoose = require('mongoose');
 mongoose.connect(config.mongoUrl);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    // we're connected!
-    console.log("Connected correctly to server");
+db.once('open', function() {
+  // we're connected!
+  console.log('Connected correctly to server');
 });
-
 
 // passport config
 var User = require('./models/user');
@@ -39,24 +35,27 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.all('/', function(req, res, next) {
+  // Allow requests from Picsily UI
+  res.header('Access-Control-Allow-Origin', 'https://picsily.herokuapp.com');
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser({ keepExtensions: true, uploadDir: "uploads",  limit: "50mb" }));
+app.use(bodyParser({ keepExtensions: true, uploadDir: 'uploads', limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
 app.use('/photos', photosRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -73,7 +72,7 @@ if (app.get('env') === 'development') {
     res.status(err.status || 500);
     res.json({
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
